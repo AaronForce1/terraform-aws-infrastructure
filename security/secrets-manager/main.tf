64cb8474bc4154e2ea1b1351e16fa8683369c85f
Aaron Baideme <AVBaideme@me.com>
2023-12-14 18:47:05 +0800

    fix: SM Secret Name

--- security/secrets-manager/main.tf
+++ security/secrets-manager/main.tf
@@ -3,7 +3,7 @@ module "secrets_manager" {
   version  = "1.1.1"
   for_each = var.secrets
 
-  name          = try(each.value.name, each.key)
+  name          = coalesce(each.value.name, each.key)
   description   = try(each.value.description, null)
   secret_string = try(each.value.value, null)
   kms_key_id    = try(each.value.key_id, null)
