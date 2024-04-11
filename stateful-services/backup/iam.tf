48df090c51051b7998f14d043649f97bcbc2727b
Aaron Baideme <avbaideme@proton.me>
2024-04-11 09:27:56 +0800

    feat: Setting up AWS Backup configuration

---
+++ stateful-services/backup/iam.tf
@@ -0,0 +1,74 @@
+data "aws_partition" "current" {}
+
+data "aws_iam_policy_document" "ab_role_assume_role_policy" {
+  count = var.iam_role_arn == null ? 1 : 0
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ab_role" {
+  count              = var.iam_role_arn == null ? 1 : 0
+  name               = var.iam_role_name == "" ? "${var.plan_name}-role" : var.iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.ab_role_assume_role_policy[0].json
+
+  tags = local.base_tags
+}
+
+resource "aws_iam_role_policy_attachment" "ab_policy_attach" {
+  count      = var.iam_role_arn == null ? 1 : 0
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+  role       = aws_iam_role.ab_role[0].name
+}
+
+resource "aws_iam_role_policy_attachment" "ab_backup_s3_policy_attach" {
+  count      = var.iam_role_arn == null ? 1 : 0
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AWSBackupServiceRolePolicyForS3Backup"
+  role       = aws_iam_role.ab_role[0].name
+}
+
+# Tag policy
+data "aws_iam_policy_document" "ab_tag_policy_document" {
+  count = var.iam_role_arn == null ? 1 : 0
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "backup:ListTags",
+      "backup:TagResource",
+      "backup:UntagResource",
+      "tag:GetResources"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ab_tag_policy" {
+  count       = var.iam_role_arn == null ? 1 : 0
+  description = "AWS Backup Tag policy"
+  policy      = data.aws_iam_policy_document.ab_tag_policy_document[0].json
+  tags        = local.base_tags
+}
+
+resource "aws_iam_role_policy_attachment" "ab_tag_policy_attach" {
+  count      = var.iam_role_arn == null ? 1 : 0
+  policy_arn = aws_iam_policy.ab_tag_policy[0].arn
+  role       = aws_iam_role.ab_role[0].name
+}
+
+# Restores policy
+resource "aws_iam_role_policy_attachment" "ab_restores_policy_attach" {
+  count      = var.iam_role_arn == null ? 1 : 0
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
+  role       = aws_iam_role.ab_role[0].name
+}
+
+resource "aws_iam_role_policy_attachment" "ab_restores_s3_policy_attach" {
+  count      = var.iam_role_arn == null ? 1 : 0
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AWSBackupServiceRolePolicyForS3Restore"
+  role       = aws_iam_role.ab_role[0].name
+}
---
+++ stateful-services/backup/main.tf
@@ -0,0 +1,104 @@
+# AWS Backup vault
+resource "aws_backup_vault" "ab_vault" {
+  count         = var.vault_name != null ? 1 : 0
+  name          = var.vault_name
+  kms_key_arn   = var.vault_kms_key_arn
+  force_destroy = var.vault_force_destroy
+  tags          = local.base_tags
+}
+
+# AWS Backup vault lock configuration
+resource "aws_backup_vault_lock_configuration" "ab_vault_lock_configuration" {
+  count               = var.locked && var.vault_name != null ? 1 : 0
+  backup_vault_name   = aws_backup_vault.ab_vault[0].name
+  changeable_for_days = var.changeable_for_days
+  max_retention_days  = var.max_retention_days
+  min_retention_days  = var.min_retention_days
+}
+
+# AWS Backup plan
+resource "aws_backup_plan" "ab_plan" {
+  count = length(local.rules) > 0 ? 1 : 0
+  name  = var.plan_name
+
+  # Rules
+  dynamic "rule" {
+    for_each = local.rules
+    content {
+      rule_name                = lookup(rule.value, "name", null)
+      target_vault_name        = lookup(rule.value, "target_vault_name", null) != null ? rule.value.target_vault_name : var.vault_name != null ? aws_backup_vault.ab_vault[0].name : "Default"
+      schedule                 = lookup(rule.value, "schedule", null)
+      start_window             = lookup(rule.value, "start_window", null)
+      completion_window        = lookup(rule.value, "completion_window", null)
+      enable_continuous_backup = lookup(rule.value, "enable_continuous_backup", null)
+      recovery_point_tags      = length(lookup(rule.value, "recovery_point_tags", {})) == 0 ? local.base_tags : lookup(rule.value, "recovery_point_tags")
+
+      # Lifecycle
+      dynamic "lifecycle" {
+        for_each = length(lookup(rule.value, "lifecycle", {})) == 0 ? [] : [lookup(rule.value, "lifecycle", {})]
+        content {
+          cold_storage_after = lookup(lifecycle.value, "cold_storage_after", 0)
+          delete_after       = lookup(lifecycle.value, "delete_after", 90)
+        }
+      }
+
+      # Copy action
+      dynamic "copy_action" {
+        for_each = lookup(rule.value, "copy_actions", [])
+        content {
+          destination_vault_arn = lookup(copy_action.value, "destination_vault_arn", null)
+
+          # Copy Action Lifecycle
+          dynamic "lifecycle" {
+            for_each = length(lookup(copy_action.value, "lifecycle", {})) == 0 ? [] : [lookup(copy_action.value, "lifecycle", {})]
+            content {
+              cold_storage_after = lookup(lifecycle.value, "cold_storage_after", 0)
+              delete_after       = lookup(lifecycle.value, "delete_after", 90)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  # Advanced backup setting
+  dynamic "advanced_backup_setting" {
+    for_each = var.windows_vss_backup ? [1] : []
+    content {
+      backup_options = {
+        WindowsVSS = "enabled"
+      }
+      resource_type = "EC2"
+    }
+  }
+
+  # Tags
+  tags = local.base_tags
+
+  # First create the vault if needed
+  depends_on = [aws_backup_vault.ab_vault]
+}
+
+locals {
+
+  # Rule
+  rule = var.rule_name == null ? [] : [
+    {
+      name              = var.rule_name
+      target_vault_name = var.vault_name != null ? var.vault_name : "Default"
+      schedule          = var.rule_schedule
+      start_window      = var.rule_start_window
+      completion_window = var.rule_completion_window
+      lifecycle = var.rule_lifecycle_cold_storage_after == null ? {} : {
+        cold_storage_after = var.rule_lifecycle_cold_storage_after
+        delete_after       = var.rule_lifecycle_delete_after
+      }
+      enable_continuous_backup = var.rule_enable_continuous_backup
+      recovery_point_tags      = var.rule_recovery_point_tags
+    }
+  ]
+
+  # Rules
+  rules = concat(local.rule, var.rules)
+
+}
